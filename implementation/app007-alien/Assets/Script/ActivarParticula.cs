48ef6b23621312f0f1364f95a38c5e3eb67128fc
Walter <walterjesusvale@gmail.com>
2014-10-01 12:59:07 -0300

    verion final alien

---
+++ implementation/app007-alien/Assets/Script/ActivarParticula.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivarParticula : MonoBehaviour {
+
+	
+	
+	public Transform particulaExplotion1;
+	private ParticleEmitter particleSystem1; 
+	 public Transform prefabExplosion;
+	private Transform elemento;
+	 public Transform marcadorPadre;
+	public Transform particulaExplotionInfinit;
+	private ParticleEmitter particleSystem2;
+	
+	
+	void Start () {
+		particleSystem2=particulaExplotionInfinit.GetComponent<ParticleEmitter>();
+		particleSystem1=particulaExplotion1.GetComponent<ParticleEmitter>();
+	}
+
+	void Update () {
+	}
+	
+
+	public void enableParticula() {
+		
+		particleSystem1.emit=true;
+		particleSystem2.emit=true;
+	    this.explotionBlood();
+		
+	}
+	public void disableParticleInfinit(){
+		particleSystem2=particulaExplotionInfinit.GetComponent<ParticleEmitter>();
+		particleSystem2.emit=false;
+	}
+	public void disableParticula() {
+		particleSystem1.emit=false;
+	
+	}
+	public void explotionBlood() {
+	    elemento= Instantiate(prefabExplosion,prefabExplosion.transform.position,prefabExplosion.rotation) as Transform;
+		elemento.parent=marcadorPadre;
+		elemento.transform.localScale=new Vector3(prefabExplosion.transform.localScale.x, prefabExplosion.transform.localScale.y, prefabExplosion.transform.localScale.z);
+	    elemento.transform.localPosition=new Vector3(prefabExplosion.transform.position.x,prefabExplosion.transform.position.y,prefabExplosion.transform.position.z);
+	    Destroy(elemento.gameObject,2);
+	
+	}
+	 
+}
---
+++ implementation/app007-alien/Assets/Script/SoundInfinit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundInfinit : MonoBehaviour {
+	public AudioClip ambient;
+	public float timeFrameSound;
+	private float frame=86f;
+	private int indexSound=1;
+	// Use this for initialization
+	void Start () {
+	  playSoundAmbient();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeFrameSound=Time.time;
+	    if(timeFrameSound>=frame*indexSound){	
+			playSoundAmbient();
+			indexSound++;
+		}
+	}
+	public void playSoundAmbient(){
+		  audio.PlayOneShot(ambient);
+		  
+	}
+}
